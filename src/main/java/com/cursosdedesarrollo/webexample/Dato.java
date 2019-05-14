4533c05e6c9ff10add9c1da3597b7efbbad8c6c6
pepesan <pepesan@gmailcom>
2019-05-14 15:17:07 +0200

    mejoras en el servicio rest para gestionar todas las operaciones típicas

--- src/main/java/com/cursosdedesarrollo/webexample/Dato.java
+++ src/main/java/com/cursosdedesarrollo/webexample/Dato.java
@@ -1,43 +1,11 @@
 package com.cursosdedesarrollo.webexample;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Dato {
+    private Integer id;
     private String cadena;
 
-    public Dato() {
-        this.cadena = "";
-    }
 
-    public Dato(String cadena) {
-        this.cadena = cadena;
-    }
-
-    public String getCadena() {
-        return cadena;
-    }
-
-    public void setCadena(String cadena) {
-        this.cadena = cadena;
-    }
-
-    @Override
-    public String toString() {
-        return "Dato{" +
-                "cadena='" + cadena + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dato dato = (Dato) o;
-        return Objects.equals(cadena, dato.cadena);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cadena);
-    }
 }
--- src/main/java/com/cursosdedesarrollo/webexample/DatoController.java
+++ src/main/java/com/cursosdedesarrollo/webexample/DatoController.java
@@ -15,6 +15,7 @@ public class DatoController {
         this.listado= new ArrayList<Dato>();
         for (int i = 0; i < 10; i++) {
             Dato dato= new Dato();
+            dato.setId(i);
             dato.setCadena("Mi cadena "+i);
             this.listado.add(dato);
         }
@@ -23,19 +24,41 @@ public class DatoController {
 
     @GetMapping
     public List<Dato> showDatos(){
+        // Coger el listado de la BBDD
         return listado;
     }
     @PostMapping
-    public Dato addDatos(Dato dato){
+    public Dato addDatos(@RequestBody Dato dato){
+        // Añadir el dato a las BBDD
         this.listado.add(dato);
+        dato.setId(this.listado.size());
         System.out.println("Dato: "+ dato);
         return dato;
     }
-    @RequestMapping("/1")
-    @GetMapping
-    public Dato showDato(){
+
+    @GetMapping("/{id}")
+    public Dato getDato(@PathVariable(value = "id") Integer id){
+        // Coger el dato de BBDD
         Dato dato = new Dato();
+        dato.setId(id);
         dato.setCadena("Hola Don pepito!!!");
+        return dato;
+    }
+
+    @PutMapping("/{id}")
+    public Dato editDato(@PathVariable(value = "id") Integer id,
+                         @RequestBody Dato dato){
+        // Modifica en la BBDD
+        dato.setId(id);
+        return dato;
+    }
+
+    @DeleteMapping("/{id}")
+    public Dato deleteDato(@PathVariable(value = "id") Integer id){
+        // Borra en la BBDD
+        Dato dato = new Dato();
+        dato.setId(id);
+        dato.setCadena("Borrado");
         return dato;
     }
 }
